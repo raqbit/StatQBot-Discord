1fd105941f204876607bd56a8b9b7bbcf3dd3e26
Raqbit <raqbit@outlook.com>
2017-05-25 16:51:23 +0200

    Added global count & stats msg

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ linkify.tlds(require('tlds'));
 const admin = require('firebase-admin');
 const serviceAccount = require('./priv/serviceAccountKey.json');
 
+const settings = require('./settings.json');
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://mancave-statistics.firebaseio.com"
@@ -15,16 +17,37 @@ const database = admin.database();
 const dbRef = database.ref('/');
 dbRef.on('value', gotData, errorData);
 
+const globalRef = dbRef.child('global');
+
+const charCountRef = dbRef.child('charCount');
+const globalCharCountRef = globalRef.child('charCount');
+
+const msgCountRef = dbRef.child('msgCount');
+const globalMsgCountRef = globalRef.child('msgCount')
+
+const wordCountRef = dbRef.child('wordCount');
+
+
 let charCount = {};
+
+let globalCharCount = 0;
+
 let msgCount = {};
+
+let globalMsgCount = 0;
+
 let wordCount = {};
+
+
 
 function gotData(snapshot) {
     var data = snapshot.val();
     if (data) {
-        charCount = data.charCount || {};
-        msgCount = data.msgCount || {};
-        wordCount = data.wordCount || {};
+        charCount = data.charCount;
+        globalCharCount = data.global.charCount;
+        msgCount = data.msgCount;
+        globalMsgCount = data.global.msgCount;
+        wordCount = data.wordCount;
     }
 }
 
@@ -37,7 +60,20 @@ const client = new Discord.Client();
 const discordKey = require('./priv/discordKey.json');
 
 client.on('message', message => {
-    if (!message.author.bot) {
+    if (message.content === '&stats') {
+        message.channel.sendMessage('', {
+            embed: new Discord.RichEmbed()
+                .setTitle('Mancave-Statistics')
+                .setDescription('**Global Stats:**\n' + formatStats())
+                .setURL('https://mancave-statistics.firebaseapp.com')
+                .setColor('#ffc800')
+                .setThumbnail('https://mancave-statistics.firebaseapp.com/favicon/mstile-310x310.png')
+                .setFooter('Click link for all statistics.')
+        });
+        return;
+    }
+
+    if (!message.author.bot && !message.channel.name === 'bot-testing' && !settings.dev) {
         upMsgCount(message.author.username);
         upCharCount(message.author.username, message.content);
         upWordCount(message.content);
@@ -53,8 +89,9 @@ function upMsgCount(username) {
         msgCount[username] = 1;
     }
 
-    const msgCountRef = dbRef.child('msgCount');
+    globalMsgCount++;
     msgCountRef.set(msgCount);
+    globalMsgCountRef.set(globalMsgCount);
 }
 
 function upCharCount(username, msg) {
@@ -64,9 +101,10 @@ function upCharCount(username, msg) {
     else {
         charCount[username] = msg.length;
     }
+    globalCharCount += msg.length;
 
-    const charCountRef = dbRef.child('charCount');
     charCountRef.set(charCount);
+    globalCharCountRef.set(globalCharCount);
 }
 
 function upWordCount(msg) {
@@ -86,7 +124,6 @@ function upWordCount(msg) {
         }
     });
 
-    const wordCountRef = dbRef.child('wordCount');
     wordCountRef.set(wordCount);
 }
 
@@ -110,4 +147,8 @@ function filterAndSplit(string) {
 
 function seededID(seed) {
     return crypto.createHash('md5').update(seed).digest('hex');
+}
+
+function formatStats() {
+    return 'Messages sent: ' + globalMsgCount + '\nCharacters sent: ' + globalCharCount + '\n';
 }
